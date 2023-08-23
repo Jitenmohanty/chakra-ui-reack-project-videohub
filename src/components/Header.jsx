37ef1076296f81ac6215c73933f8291c8529c715
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-08-23 19:25:50 +0530

    ChkraUi uses in react

---
+++ src/components/Header.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import {
+    Drawer,
+    DrawerBody,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+    Button,
+    VStack,
+    HStack,
+  } from '@chakra-ui/react';
+  import { Link } from 'react-router-dom';
+import { BiMenuAltLeft } from 'react-icons/bi';
+
+const Header = () => {
+
+    const {isOpen,onOpen , onClose} = useDisclosure();
+  return (
+    <>
+      <Button
+      pos={'fixed'}
+      top={3}
+      left={4}
+      colorScheme='purple'
+      p={0}
+      w={10}
+      h={10}
+      borderRadius={'full'}
+      onClick={onOpen}
+      >
+        <BiMenuAltLeft size={'20'}/>
+      </Button>
+      <Drawer isOpen={isOpen} placement='left' onClose={onClose}>
+        <DrawerOverlay/>
+        <DrawerContent>
+        <DrawerCloseButton/>
+        <DrawerHeader>QUICK HUB</DrawerHeader>
+        <DrawerBody>
+            <VStack alignItems={'flex-start'}>
+                <Button variant={'ghost'} colorScheme={'purple'}>
+                    <Link to={'/'}>Home</Link>
+                </Button>
+                <Button variant={'ghost'} colorScheme={'purple'}>
+                    <Link to={'/videos'}>Videos</Link>
+                </Button>
+                <Button variant={'ghost'} colorScheme={'purple'}>
+                    <Link to={'/videos?catagory=free'}>Free Videos</Link>
+                </Button>
+                <Button variant={'ghost'} colorScheme={'purple'}>
+                    <Link to={'/upload'}>Upload Video</Link>
+                </Button>
+            </VStack>
+            <HStack pos={'absolute'} bottom={10} left={0} justifyContent={'space-evenly'} w={'full'}>
+                <Button colorScheme={'purple'}>
+                    <Link to={'/login'}>Login</Link>
+                </Button>
+                <Button colorScheme={'purple'} variant={'outline'}>
+                    <Link to={'/signup'}>Sign Up</Link>
+                </Button>
+            </HStack>
+        </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
+
+export default Header
